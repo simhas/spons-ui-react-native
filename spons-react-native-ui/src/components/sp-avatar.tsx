aca8e141445c55e7d4269a4b9be6cb82bb46c610
Simen <simen.hasselknippe@live.com>
2025-04-11 23:35:12 +0200

    Update

--- spons-react-native-ui/src/components/sp-avatar.tsx
+++ spons-react-native-ui/src/components/sp-avatar.tsx
@@ -1,4 +1,6 @@
 import { Image } from 'expo-image';
+import { View, Text } from 'react-native';
+import { useTheme } from '../theme-provider';
 
 interface SpAvatarProps {
     url?: string | null
@@ -8,12 +10,33 @@ interface SpAvatarProps {
 }
 
 export function SpAvatar(props: SpAvatarProps) {
+
+    const theme = useTheme();
     const { url = null, name = "", size = 48, backgroundColor = null } = props
 
-    var uri = url ? url : `https://api.dicebear.com/9.x/initials/svg?seed=${name}&backgroundType=solid,gradientLinear&scale=80`;
+    function getColorFromText(text: string) {
+        const hash = text.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+        const hue = hash % 360;
+        return `hsl(${hue}, 100%, 35%)`;
+    }
 
-    if (backgroundColor != null && url == null) {
-        uri = uri + `&backgroundColor=${backgroundColor}`;
+    if (url == null) {
+        return (
+            <View
+                style={{
+                    width: size,
+                    height: size,
+                    borderRadius: 999,
+                    backgroundColor: backgroundColor ?? getColorFromText(name),
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <Text style={{ color: theme.colors.background, fontSize: 18, fontWeight: '600' }}>
+                    {name.substring(0, 2).toUpperCase()}
+                </Text>
+            </View>
+        )
     }
 
     return (
@@ -24,7 +47,7 @@ export function SpAvatar(props: SpAvatarProps) {
                 aspectRatio: 1 / 1,
                 borderRadius: 999,
             }}
-            source={{ uri: uri }}
+            source={{ uri: url }}
             placeholder={{ blurhash: '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[' }}
             contentFit="cover"
             contentPosition={"center"}
